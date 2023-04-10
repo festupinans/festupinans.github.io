b14264cf502001339e98f8e0939b115ca1ddfdac
Francisco Estupiñán <90729579+festupinans@users.noreply.github.com>
2023-04-09 23:11:51 -0500

    Responsive Loader

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ const loaderContainer = document.getElementById('loader-container');
 
 continiun.addEventListener('click', () => {
     event.preventDefault();
-    gsap.to(loaderContainer, { y: "-100vh", duration: 1 });
+    gsap.to(loaderContainer, { y: "-120vh", duration: 1 });
+    window.scrollTo(0, 0);
 })
 
 contacto.addEventListener('click', () => {
     event.preventDefault();
     gsap.to(loaderContainer, { y: "0vh", duration: 1 });
+    window.scrollTo(0, 0);
 })

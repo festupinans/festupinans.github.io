8db70aaf80ce72fe39f446ac426894b19e30b54e
Francisco Estupiñán <90729579+festupinans@users.noreply.github.com>
2026-01-24 16:31:41 -0500

    Revert "add netlify"
    This reverts commit e0b20acdcd746dc05388087d5f0c13314ce2aed0.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import netlify from '@astrojs/netlify';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://festupinans.github.io',
-  base: '/festupinans.github.io',
-  adapter: netlify(),
+    site: 'https://festupinans.github.io',
+    base: '/festupinans.github.io',
 });

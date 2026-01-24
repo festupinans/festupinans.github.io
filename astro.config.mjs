0c01d3126147a50c0be4a19a91f68a528a928c44
Francisco Estupiñán <90729579+festupinans@users.noreply.github.com>
2026-01-24 16:31:37 -0500

    Revert "Netlify config"
    This reverts commit de02fd664c7c746cb9eb07248ed2b415bb21ca1f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://festupinans.github.io',
+  base: '/festupinans.github.io',
   adapter: netlify(),
 });

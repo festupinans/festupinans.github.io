9a9d28fe0f39762ba8ab8171fcdabf81ee48ec0e
Francisco Estupiñán <90729579+festupinans@users.noreply.github.com>
2026-01-24 13:18:12 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://festupinans.github.io',
-    base: 'festupinans.github.io',
+    base: '/festupinans.github.io',
 });
